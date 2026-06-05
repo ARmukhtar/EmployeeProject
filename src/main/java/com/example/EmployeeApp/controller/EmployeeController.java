b05d390443b428fdddd951be8ea21a00602c2126
ARMukhtar <adebisiramadan9@gmail.com>
2026-06-05 15:29:40 +0100

    Enhance CORS configuration in EmployeeController

--- src/main/java/com/example/EmployeeApp/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeApp/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = "https://armukhtar.github.io")  // ← ADD THIS LINE
+@CrossOrigin(origins = "https://armukhtar.github.io", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class EmployeeController {
 
     @Autowired
